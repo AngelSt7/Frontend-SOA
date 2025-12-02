675510ccbe954e9b4a0c8a12e5f2cfa530525ae2
AngelSt7 <santacruz232004@gmail.com>
2025-12-02 14:24:43 -0500

    feat: update files

--- obj/Debug/net8.0/WebVue.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebVue.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebVue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4d52ca28664e7f8e07ac1743363063844118d6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83e267e43a907b474b740f01b815c2b955a5557f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebVue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebVue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
